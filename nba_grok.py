e6c0aed936bf925b863eaa8b930c7b4bca2af328
Bill Ghai <bsghai@gmail.com>
2025-03-21 09:18:42 -0700

    set Flask to bind to 0.0.0.0 for Rebder

--- nba_grok.py
+++ nba_grok.py
@@ -47,5 +47,5 @@ def index():
     return render_template('index.html', response=None)
 
 if __name__ == '__main__':
-    app.run(debug=False)
-    
+    port = int(os.getenv("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
